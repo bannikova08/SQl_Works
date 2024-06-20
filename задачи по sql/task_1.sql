0075abe366bf131677d33163aed1fbb8ad914ec6
bannikova08 <126084807+bannikova08@users.noreply.github.com>
2024-06-20 08:04:23 +0300

    Update task_1.sql

--- задачи по sql/task_1.sql
+++ задачи по sql/task_1.sql
@@ -4,8 +4,8 @@
 датой начала турнира в этом году, вывести её.*/
 
 SELECT TO_CHAR(TO_DATE('&&N','DD.MM.SYYYY'), 'DD-MON-SYYYY', 'NLS_DATE_LANGUAGE = ENGLISH')  AS "Заданная дата", /*ВВОДИМ ЗАДАННУЮ ДАТУ*/
-CASE
 
+CASE
 WHEN TO_DATE('&N','DD.MM.SYYYY') <= TO_DATE('23.06.1877', 'DD.MM.SYYYY')THEN TO_CHAR(TO_DATE('23.06.1877', 'DD.MM.SYYYY'), 'DD-MON-SYYYY', 'NLS_DATE_LANGUAGE = ENGLISH')
 /*ЕСЛИ ДАТА МЕНЬШЕ ПЕРВОГО ТУРНИРА, ТО ВЫВОДИМ ДАТУ ПЕРВОГО ТУРНИРА*/
 WHEN TO_DATE('&N','DD.MM.SYYYY') > TO_DATE('21.06.9999', 'DD.MM.SYYYY')
@@ -18,5 +18,6 @@ THEN TO_CHAR(NEXT_DAY(TO_DATE(CONCAT('01.08',TO_CHAR(TO_NUMBER(TO_CHAR(TO_DATE('
 ELSE TO_CHAR(NEXT_DAY(TO_DATE(CONCAT('01.08',(TO_CHAR(TO_DATE('&N', 'DD.MM.SYYYY'),'SYYYY')))),'Понедельник')-42, 'DD-MON-SYYYY', 'NLS_DATE_LANGUAGE = ENGLISH') END "Дата начала турнира"
 /*ИНАЧЕ ИЩЕМ БЛИЖАЙШИЙ ТУРНИР, КОТОРЫЙ ПРОЙДЕТ ЭТИМ ЛЕТОМ*/
 FROM DUAL;
+
 UNDEFINE N;
 /*УДАЛЯЕМ ЗНАЧЕНИЕ ПЕРЕМЕННОЙ*/
